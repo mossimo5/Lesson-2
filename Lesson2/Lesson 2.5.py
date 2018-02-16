86c7f81e24ad8cad3c5e6b2f5be4e971d5729da6
mossimo5 <36167029+mossimo5@users.noreply.github.com>
2018-02-16 08:38:06 -0800

    Add files via upload

---
+++ Lesson2/Lesson 2.5.py
@@ -0,0 +1,19 @@
+# Jack G. Bingham --- Lesson 2.5
+
+degrees = input("Enter Temp in F: ")
+
+degrees = float(degrees)
+
+step_one = degrees - 32
+
+print (step_one)
+
+step_two = step_one * 5
+
+print (step_two)
+
+step_three = step_two//9
+
+print (step_three)
+
+print ("Degrees Celsius: {}".format(degrees))
