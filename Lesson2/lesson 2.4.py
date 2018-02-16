2f745441c1070180107b25866eae15c299a85959
mossimo5 <36167029+mossimo5@users.noreply.github.com>
2018-02-16 08:29:18 -0800

    Add files via upload

---
+++ Lesson2/lesson 2.4.py
@@ -0,0 +1,13 @@
+# Jack G. Bingham --- Lesson 2.4
+
+hours = input("How many hours? ")
+
+hours = float(hours)
+
+rate = input("What is the rate? ")
+
+rate = float(rate)
+
+pay = hours * rate
+
+print ("Money earned: {}".format(pay)) 
